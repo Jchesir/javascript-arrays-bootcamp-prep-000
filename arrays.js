b6c98f7cc8494ac662dd4915f165ae82ff62bb18
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-04-02 15:39:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-fvar chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
   var newarray = [element, ...array]
